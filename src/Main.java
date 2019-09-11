4c133f02ad01439c840df63e6ab7983f8c01d5bc
Florin <briflorin19@gmail.com>
2019-09-11 20:51:13 +0300

    test

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.awt.desktop.AboutEvent;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");}
+
+        // Tipuri de date
+    String brand  = "Acer";
+    String mdel = "Aspire";
+
+
+
+}
+

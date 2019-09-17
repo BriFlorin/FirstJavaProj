f870107c3f1bae0bd6af64869500bbc93d4eddf6
Florin <briflorin19@gmail.com>
2019-09-17 11:40:27 +0300

    Homework-introduction into java

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,64 @@ import java.awt.desktop.AboutEvent;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");}
+        String exercise1 = "Ex. 1";
+        System.out.println(exercise1);
 
-        // Tipuri de date
-    String brand  = "Acer";
-    String mdel = "Aspire";
+        String greeting="Hello";
+        String name="Bri Florin";
+        System.out.println(greeting);
+        System.out.println(name);
 
+        String exercise2 = "Ex. 2";
+        System.out.println(exercise2);
+        int x = 20;
+        int y = 40;
+        int sum = x+y;
+        System.out.println(sum);
+
+        String exercise3 = "Ex. 3";
+        System.out.println(exercise3);
+        float a = 7;
+        float b = 3;
+        float division=a/b;
+        System.out.println(division);
+
+        String exercise4a = "Ex. 4a";
+        System.out.println(exercise4a);
+        int c = -5;
+        int d = 8;
+        int e = 6;
+        int op1 = c + d *e;
+        System.out.println(op1);
+
+        String exercise4b = "Ex. 4b";
+        System.out.println(exercise4b);
+        int f = 55;
+        int g = 9;
+        int op2 = (f+g) % g;
+        System.out.println(op2);
+
+        String exercise4c = "Ex. 4c";
+        System.out.println(exercise4c);
+        int i = 20;
+        int j = -3;
+        int k = 5;
+        int l = 8;
+        float op3 = i+ (float) j*k/l;
+        System.out.println(op3);
+
+        String exercise4d = "Ex. 4d";
+        System.out.println(exercise4d);
+        a = 5;
+        b = 15;
+        c = 3;
+        d = 2;
+        e = 8;
+        f = 3;
+        float op4 = a + b / c * d - e % f;
+        System.out.println(op4);
+
+    }
 
 
 }

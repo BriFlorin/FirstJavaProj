b59134f59ce3ddcdd7f165d97515d83cb84341a8
Florin <briflorin19@gmail.com>
2019-10-12 20:40:30 +0300

    Homework methods ex3

---
+++ src/lab1/HomeworkEx3.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class HomeworkEx3 {
+    public static void printjava(){
+        String row1 ="   J    a  v     v  a";
+        String row2 ="   J   a a  v   v  a a";
+        String row3 ="J  J  aaaaa  V V  aaaaa";
+        String row4 = " JJ  a     a  V  a     a";
+        System.out.println(row1);
+        System.out.println(row2);
+        System.out.println(row3);
+        System.out.println(row4);
+    }
+    public static void main(String[] args) {
+        printjava();
+    }
+
+}

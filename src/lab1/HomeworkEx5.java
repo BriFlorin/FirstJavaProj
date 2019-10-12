1eb334ee07e70ab5b2a021a270e734b3ee277ca9
Florin <briflorin19@gmail.com>
2019-10-12 21:27:47 +0300

    Homework methods Ex5

---
+++ src/lab1/HomeworkEx5.java
@@ -0,0 +1,21 @@
+package lab1;
+
+public class HomeworkEx5 {
+    public static void robocop(){
+        String row1 =" +\"\"\"\"\"+  ";
+        String row2 ="[| O O |]";
+        String row3 =" |  ^  |";
+        String row4 =" | '-' |";
+        String row5 =" +-----+";
+        System.out.println(row1);
+        System.out.println(row2);
+        System.out.println(row3);
+        System.out.println(row4);
+        System.out.println(row5);
+
+
+    }
+    public static void main(String[] args) {
+        robocop();
+    }
+}

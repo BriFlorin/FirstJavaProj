8122b7e1f18397841e3fe93d9699b6b25023bf3f
Florin <briflorin19@gmail.com>
2019-10-12 21:39:57 +0300

    Homework methods Ex6

---
+++ src/lab1/HomeworkEx6.java
@@ -0,0 +1,16 @@
+package lab1;
+
+public class HomeworkEx6 {
+    public static int calculatedDivision (int nr1,int nr2) {
+        int rest= nr1%nr2;
+        return rest;
+
+
+    }
+    public static void main(String[] args){
+        int result = calculatedDivision( 58659595,161);
+        System.out.println(result);
+    }
+
+
+}
